684574384d2563c93d4d24722b10c85497aad48c
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-10-13 17:58:18 +0300

    fix any issue in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,7 +17,7 @@ export function formatNumberWithDecimals(num: number): string {
 }
 
 // Format errors
-// eslint-disabled-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function formatError(error: any) {
 	if (error.name === 'ZodError') {
 		// Handle Zod error
