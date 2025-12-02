6357799859848e277a47e35be68cffe004e7e9a1
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-12-02 15:28:22 +0200

    fix: add SessionProvider to user layout

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -6,27 +6,30 @@ import Link from 'next/link';
 import MainNav from './main-nav';
 import { extractRouterConfig } from 'uploadthing/server';
 import { ourFileRouter } from '../api/uploadthing/core';
+import { SessionProvider } from 'next-auth/react';
 
 export default function UserLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className="flex flex-col">
-			<div className="border-b container mx-auto">
-				<div className="flex items-center h-16 px-4">
-					<Link href="/">
-						<Image src="/images/logo.svg" width={48} height={48} alt={APP_NAME} />
-					</Link>
-					<MainNav className="mx-6" />
+		<SessionProvider refetchInterval={0} refetchOnWindowFocus={true}>
+			<div className="flex flex-col">
+				<div className="border-b container mx-auto">
+					<div className="flex items-center h-16 px-4">
+						<Link href="/">
+							<Image src="/images/logo.svg" width={48} height={48} alt={APP_NAME} />
+						</Link>
+						<MainNav className="mx-6" />
 
-					<div className="ml-auto flex items-center space-x-4">
-						<Menu />
+						<div className="ml-auto flex items-center space-x-4">
+							<Menu />
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<main className="flex-1 container mx-auto pt-6 p-8 space-y-4">
-				<NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-				{children}
-			</main>
-		</div>
+				<main className="flex-1 container mx-auto pt-6 p-8 space-y-4">
+					<NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+					{children}
+				</main>
+			</div>
+		</SessionProvider>
 	);
 }
