08a65e6114a79fd0d75921ca5dee0d57dc5c922b
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-12-02 15:17:17 +0200

    fix: add sessionProvider to admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -10,27 +10,29 @@ import { ourFileRouter } from '../api/uploadthing/core';
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className="flex flex-col">
-			<div className="border-b container mx-auto">
-				<div className="flex items-center h-16 px-4">
-					<Link href="/">
-						<Image src="/images/logo.svg" width={48} height={48} alt={APP_NAME} />
-					</Link>
-					<MainNav className="mx-6" />
+		<SessionProvider refetchInterval={0} refetchOnWindowFocus={true}>
+			<div className="flex flex-col">
+				<div className="border-b container mx-auto">
+					<div className="flex items-center h-16 px-4">
+						<Link href="/">
+							<Image src="/images/logo.svg" width={48} height={48} alt={APP_NAME} />
+						</Link>
+						<MainNav className="mx-6" />
 
-					<div className="ml-auto flex items-center space-x-4">
-						<div className="md:w-[100px] lg:w-[300px]">
-							<AdminSearch />
+						<div className="ml-auto flex items-center space-x-4">
+							<div className="md:w-[100px] lg:w-[300px]">
+								<AdminSearch />
+							</div>
+							<Menu />
 						</div>
-						<Menu />
 					</div>
 				</div>
-			</div>
 
-			<main className="flex-1 container mx-auto pt-6 p-8 space-y-4">
-				<NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-				{children}
-			</main>
-		</div>
+				<main className="flex-1 container mx-auto pt-6 p-8 space-y-4">
+					<NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+					{children}
+				</main>
+			</div>
+		</SessionProvider>
 	);
 }
